ab991590d7584821d381d86906f11c3dd3cf2048
Ughlympics <kipgora@gmail.com>
2025-03-25 17:53:28 +0200

    Lab2_viz_cipher

---
+++ Lab2/Vigenеre_cipher.py
@@ -0,0 +1,38 @@
+import sys
+
+def vigenere_encrypt(text, key):
+    alphabet = "абвгдежзийклмнопрстуфхцчшщьыъэюя"
+    text = text.lower().replace("ё", "е")
+    key = key.lower().replace("ё", "е")
+    encrypted_text = ""
+    key_index = 0
+
+    for char in text:
+        if char in alphabet:
+            shift = alphabet.index(key[key_index % len(key)])
+            new_letter = alphabet[(alphabet.index(char) + shift) % len(alphabet)]
+            encrypted_text += new_letter
+            key_index += 1
+        else:
+            encrypted_text += char
+    
+    return encrypted_text
+
+def process_file(input_filename, output_filename, key):
+    try:
+        with open(input_filename, 'r', encoding='utf-8') as infile:
+            text = infile.read()
+        encrypted_text = vigenere_encrypt(text, key)
+        with open(output_filename, 'w', encoding='utf-8') as outfile:
+            outfile.write(encrypted_text)
+        print(f"The file has been successfully encrypted and saved in {output_filename}")
+    except FileNotFoundError:
+        print("Error: Input file not found.")
+
+if __name__ == "__main__":
+    input_file = "random_text.txt"
+    output_file = "random_text_2.txt"
+    key = "бо"
+    process_file(input_file, output_file, key)
+
+#шифрувалося повідомленням: богородицабогсынбожи
